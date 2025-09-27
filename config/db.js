4aab725c5b9d71f28a2dac45b0b332acda9b9298
Aishwara <charuwinda1998@gmail.com>
2025-09-27 13:22:52 +0530

    Update package.json for Render compatibility & update db.js for Aiven SSL and Render deployment

--- config/db.js
+++ config/db.js
@@ -1,10 +1,25 @@
 import { Sequelize } from 'sequelize';
-require('dotenv').config();
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-  host: process.env.MYSQL_HOST,
-  dialect: 'mysql',
-  logging: false,
-});
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT || 3306,
+    dialect: 'mysql',
+    ssl: true, 
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true 
+      }
+    },
+    logging: false,
+  }
+);
 
 export default sequelize;
