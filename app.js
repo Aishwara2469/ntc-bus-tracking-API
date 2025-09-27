647dcfab573b175edb4a2993b3d5876da9e5d9db
Aishwara <charuwinda1998@gmail.com>
2025-09-27 12:16:40 +0530

    add swagger-ui-express for API documentation

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ import routeRoutes from './routes/routeRoutes.js';
 import busRoutes from './routes/busRoutes.js';
 import tripRoutes from './routes/tripRoutes.js';
 import locationRoutes from './routes/locationRoutes.js';
+import swaggerUi from 'swagger-ui-express';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 
 const app = express();
 app.use(express.json());
+
+const openapiSpec = JSON.parse(await readFile(join(process.cwd(), 'docs', 'openapi.json'), 'utf8'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 app.use('/auth', authRoutes);
 app.use('/routes', routeRoutes);
@@ -16,6 +22,7 @@ app.use('/trips', tripRoutes);
 app.use('/locations', locationRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 sequelize.authenticate()
   .then(() => {
     console.log('Database connected');
